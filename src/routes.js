1d5ec626726fd02e0feee62314fe4757dbdab98d
romuloms <romulomsantana@hotmail.com>
2023-05-23 17:59:11 -0300

    created Category repository and controller

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,11 @@
 const { Router } = require('express');
 
 const ContactController = require('./app/controllers/ContactController');
+const CategoryController = require('./app/controllers/CategoryController');
 
 const router = Router();
 
-router.get(
-  '/contacts',
-  (request, response, next) => {    // - Middleware -
-    request.appId = 'MeuAppId';
-    // response.send('interceptado pelo middleware');
-    next();
-  },
-  ContactController.index,
-);
+router.get('/contacts/:id', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts', ContactController.store);
@@ -20,5 +13,8 @@ router.put('/contacts/:id', ContactController.update);
 
 // o Express entende as rotas como se fossem middlewares, por isso o uso do app.use
 // execucao do Express: Midd 1 -> Midd 2 -> Midd 3 -> ... -> Midd n (Route)
+
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
 
 module.exports = router;
