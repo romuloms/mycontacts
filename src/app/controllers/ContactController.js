88490e4d4d6a5468321074e8a0252997b854bf1a
romuloms <romulomsantana@hotmail.com>
2023-05-23 17:38:34 -0300

    refactored ContactsRepository delete method

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -78,13 +78,6 @@ class ContactController {
     // deletar um registro
     const { id } = request.params;
 
-    const contact = await ContactsRepository.findById(id);
-
-    if (!contact) {
-      // 404: Not Found
-      return response.status(404).json({ error: 'User not found' });
-    }
-
     await ContactsRepository.delete(id);
     response.sendStatus(204);   // 204: No Content
   }
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid'); // universal unique ID
-
 const db = require('../../database/index');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Romulo',
-    email: 'romulo@mail.com',
-    phone: '12112424',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Jose',
-    email: 'jose@mail.com',
-    phone: '21241231',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -62,11 +43,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 }
 
