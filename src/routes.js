40414caecb53247a0624034924505bb4872ceb57
romuloms <romulomsantana@hotmail.com>
2023-05-24 12:52:34 -0300

    created Category update method

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const CategoryController = require('./app/controllers/CategoryController');
 
 const router = Router();
 
-router.get('/contacts/:id', ContactController.index);
+router.get('/contacts/', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts', ContactController.store);
@@ -15,7 +15,9 @@ router.put('/contacts/:id', ContactController.update);
 // execucao do Express: Midd 1 -> Midd 2 -> Midd 3 -> ... -> Midd n (Route)
 
 router.get('/categories', CategoryController.index);
+router.get('/categories/:id', CategoryController.show);
 router.post('/categories', CategoryController.store);
 router.delete('/categories/:id', CategoryController.delete);
+router.put('/categories/:id', CategoryController.update);
 
 module.exports = router;
