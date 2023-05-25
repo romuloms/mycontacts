254caf600deb3c2b941c53fe9b5b08f5acb45500
romuloms <romulomsantana@hotmail.com>
2023-05-25 13:18:57 -0300

    implemented express error handler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -8,5 +9,13 @@ app.use(express.json());  // permite que a API receba requisicoes com o body
 // a ordem de implementacao importa. se a linha acima viesse depois o "app.use(routes)",
 // não iria funcionar
 app.use(routes);
+
+// Error Handler Middleware (lembrar que a ordem de implementacao dele importa):
+app.use((error, request, response, next) => {
+  // esses 4 argumentos precisam ser passados e tem que ser nessa ordem
+  console.log('### Error Handler ###');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
